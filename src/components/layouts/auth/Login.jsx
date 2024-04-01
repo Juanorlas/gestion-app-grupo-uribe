93251f969a82649b268130101f9ed5c32a04a8f7
Juanorlas <juan7883@gmail.com>
2024-04-01 16:43:28 -0500

    Se crea funcion para login

--- src/components/layouts/auth/Login.jsx
+++ src/components/layouts/auth/Login.jsx
@@ -1,25 +1,29 @@
-import userLogo from '../../../assets/user.avif'
+import userLogo from "../../../assets/user.avif";
+import { useState } from "react";
 
 const Login = () => {
-  return (
+    const [getUsuario, setUsuario] = useState("")
+    const [getContrasena, setContrasena] = useState("")
+    const [getCorreo, setCorreo] = useState("")
+    return (
     <form className="login-form">
-        <img src={userLogo} alt="Usuario" />
-        <h2>Iniciar sesión</h2>
-        <div className="input-group">
-          <label htmlFor="username">Usuario:</label>
-          <input type="text" name="" id="" />
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Contraseña:</label>
-          <input type="text" name="" id="" />
-        </div>
-        <div className="input-group">
-          <label htmlFor="email">Correo electrónico:</label>
-          <input type="text" name="" id="" />
-        </div>
-        <button type="submit">Iniciar sesión</button>
-      </form>
-  )
-}
+      <img src={userLogo} alt="Usuario" />
+      <h2>Iniciar sesión</h2>
+      <div className="input-group">
+        <label htmlFor="username">Usuario:</label>
+        <input type="text" name="" id="" onChange={ (e) => {setUsuario(e.target.value)}} />
+      </div>
+      <div className="input-group">
+        <label htmlFor="password">Contraseña:</label>
+        <input type="text" name="" id="" onChange={ (e) => {setContrasena(e.target.value)}}  />
+      </div>
+      <div className="input-group">
+        <label htmlFor="email">Correo electrónico:</label>
+        <input type="text" name="" id="" onChange={ (e) => {setCorreo(e.target.value)}}  />
+      </div>
+      <button type="submit">Iniciar sesión</button>
+    </form>
+  );
+};
 
-export default Login
+export default Login;
